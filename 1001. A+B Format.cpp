48ebd24d700ee12546a093960acbf8554b9dd5a9
zhoujf <zhoujf620@zju.edu.cn>
2020-01-06 10:49:03 +0800

    commited on 20-01-06

--- 1001. A+B Format.cpp
+++ 1001. A+B Format.cpp
@@ -1,22 +1,17 @@
 // 1001 A+B Format (20分)
-// Calculate a+b and output the sum in standard format -- that is, the digits must be separated into groups of three by commas (unless there are less than four digits).
 
-// Input Specification:
-// Each input file contains one test case. Each case contains a pair of integers a and b where −10
-// ​6
-// ​​ ≤a,b≤10
-// ​6
-// ​​ . The numbers are separated by a space.
+// Calculate a + b and output the sum in standard format — that is, the digits must be separated into groups of three by commas (unless there are less than four digits).
 
-// Output Specification:
+// Input
+// Each input file contains one test case. Each case contains a pair of integers a and b where -1000000 <= a, b <= 1000000. The numbers are separated by a space.
+
+// Output
 // For each test case, you should output the sum of a and b in one line. The sum must be written in the standard format.
 
-// Sample Input:
+// Sample Input
 // -1000000 9
 
-      
-    
-// Sample Output:
+// Sample Output
 // -999,991
 
 #include <iostream>
